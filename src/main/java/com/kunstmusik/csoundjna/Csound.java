2d1ec028b3372f0d7827be042ee2cd60a6e23e9b
Steven Yi <stevenyi@gmail.com>
2018-05-20 20:59:51 -0400

    implemented minimal wrapping to get code compiling and running with Csound

--- src/main/java/com/kunstmusik/csoundjna/Csound.java
+++ src/main/java/com/kunstmusik/csoundjna/Csound.java
@@ -14,21 +14,94 @@ import com.sun.jna.Pointer;
  * @author stevenyi
  */
 public class Csound {
-    
-    public interface LibCsound extends Library {
-        LibCsound INSTANCE = (LibCsound)Native.loadLibrary("csound64", LibCsound.class);
 
-        Pointer csoundCreate(long l); 
-        int csoundGetVersion(Pointer p);
+    public static final int CSOUNDINIT_NO_SIGNAL_HANDLER = 1;
+    public static final int CSOUNDINIT_NO_ATEXIT = 2;
+
+    private static final CsoundLib INSTANCE;
+
+    static {
+        // not sure this is best as it doesn't account for if libcsound64 
+        // is not found
+        INSTANCE = (CsoundLib) Native.loadLibrary("csound64", CsoundLib.class);
+    }
+
+    public static int initialize(int flags) {
+        return INSTANCE.csoundInitialize(flags);
     }
 
     private final Pointer csoundPtr;
 
-    public Csound(){
-        csoundPtr = LibCsound.INSTANCE.csoundCreate(0);
+    public Csound() {
+        csoundPtr = INSTANCE.csoundCreate(0);
     }
 
     public int getVersion() {
-        return LibCsound.INSTANCE.csoundGetVersion(csoundPtr);
+        return INSTANCE.csoundGetVersion(csoundPtr);
     }
+
+    public int setOption(String option) {
+        return INSTANCE.csoundSetOption(csoundPtr, option);
+    }
+
+    public int compileOrc(String s) {
+        return INSTANCE.csoundCompileOrc(csoundPtr, s);
+    }
+
+    public int compileOrcAsync(String s) {
+        return INSTANCE.csoundCompileOrcAsync(csoundPtr, s);
+    }
+
+    public int compileCsdText(String csdText) {
+        return INSTANCE.csoundCompileCsdText(csoundPtr, csdText);
+    }
+
+    public void start() {
+        INSTANCE.csoundStart(csoundPtr);
+    }
+
+    public void stop() {
+        INSTANCE.csoundStop(csoundPtr);
+    }
+
+    public int performKsmps() {
+        return INSTANCE.csoundPerformKsmps(csoundPtr);
+    }
+
+    public int cleanup() {
+        return INSTANCE.csoundCleanup(csoundPtr);
+    }
+
+    public void reset() {
+        INSTANCE.csoundReset(csoundPtr);
+    }
+
+    interface CsoundLib extends Library {
+
+        Pointer csoundCreate(long l);
+
+        int csoundGetVersion(Pointer p);
+
+        int csoundInitialize(int flags);
+
+        int csoundSetOption(Pointer p, String option);
+
+        int csoundCompileOrc(Pointer p, String str);
+
+        int csoundCompileOrcAsync(Pointer p, String str);
+
+        int csoundCompileCsdText(Pointer p, String csd_text);
+
+        int csoundPerformKsmps(Pointer p);
+
+        int csoundStart(Pointer p);
+
+        void csoundStop(Pointer p);
+
+        int csoundCleanup(Pointer p);
+
+        void csoundReset(Pointer p);
+
+    }
+
 }
--- src/main/java/com/kunstmusik/csoundjna/CsoundJNA.java
+++ src/main/java/com/kunstmusik/csoundjna/CsoundJNA.java
@@ -5,14 +5,57 @@
  */
 package com.kunstmusik.csoundjna;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author stevenyi
  */
 public class CsoundJNA {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
+        // This pretty much needs to always happen when in a JVM app;
+        // maybe should just do this once within the static initializer block
+        // in Csound class...
+        Csound.initialize(Csound.CSOUNDINIT_NO_ATEXIT
+                | Csound.CSOUNDINIT_NO_SIGNAL_HANDLER);
+
         Csound csound = new Csound();
         System.out.println("Csound Version: " + csound.getVersion());
+
+        csound.setOption("-odac");
+        csound.setOption("-b1024");
+        csound.setOption("-b4096");
+        csound.compileOrc("sr=48000\nksmps=64\nnchnls=2\n0dbfs=1\n"
+                + "instr 1\n"
+                + "asig = vco2(0.25, p4)\n"
+                + "asig = diode_ladder(asig, expon(10000, p3, 100), 4)\n"
+                + "outc(asig, asig)\n"
+                + "endin\n"
+                + "schedule(1,0,10, 440)\n"
+                + "schedule(1,0,10, 880)\n"
+                + "event_i(\"e\",11,11)\n"
+        );
+        csound.start();
+
+        Thread t = new Thread(() -> {
+
+            while (csound.performKsmps() == 0) {
+            }
+            csound.stop();
+
+            csound.cleanup();
+
+            csound.reset();
+        });
+        t.start();
+
+        try {
+            t.join();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(CsoundJNA.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 }
